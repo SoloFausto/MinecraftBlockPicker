9122169777651535dfd48d1e7a4ca009cb3159bc
Fausto De Leon <fdeleon@colgate.edu>
2024-06-23 15:56:18 -0300

    Made the Percentage view responsive to the data it's given, simplified the generated code, calculated the average color for the PercentageView component for its background.

--- Models/Blocks.cs
+++ Models/Blocks.cs
@@ -2,6 +2,11 @@
     public class Blocks {
         public string? file_name { get; set; }
         public string? image_base64 { get; set; }
+        public Blocks(string? file_name, string image_base64) { 
+            this.file_name = file_name;
+            this.image_base64 = image_base64;
+        }
     }
+
 }   
 
